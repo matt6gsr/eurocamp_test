40cd748515e664bc0a452b2da0fdfbaa68815609
matt6gsr <matt6gsr@gmail.com>
2024-11-05 17:47:07 +0000

    removed logic, added margin to table header, passed props to Datatable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,55 +5,8 @@ import { Booking, columns as bookingColumns } from "./bookings/columns";
 import { Parc, columns as parcColumns } from "./parcs/columns";
 import { User, columns as userColumns } from "./users/columns";
 import { DataTable } from "../components/ui/data-table";
-
-async function getBookings(): Promise<Booking[]> {
-  return [
-    {
-      id: "12345",
-      user: "abcde",
-      parc: "67890",
-      bookingdate: "01-01-2030",
-      comments: "This is a comment",
-    },
-    {
-      id: " 54321",
-      user: "edcba",
-      parc: "09876",
-      bookingdate: "01-01-2030",
-      comments: "This is another comment",
-    },
-  ];
-}
-
-async function getParcs(): Promise<Parc[]> {
-  return [
-    {
-      id: "12345",
-      name: "Parc 1",
-      description: "This is a description",
-    },
-    {
-      id: "54321",
-      name: "Parc 2",
-      description: "This is another description",
-    },
-  ];
-}
-
-async function getUsers(): Promise<User[]> {
-  return [
-    {
-      id: "12345",
-      name: "User 1",
-      email: "user1@eurocamp.com",
-    },
-    {
-      id: "54321",
-      name: "User 2",
-      email: "user2@eurocamp.com",
-    },
-  ];
-}
+import { getBookings, getParcs, getUsers } from "../helpers/helper-functions";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   const [bookingData, setBookingData] = useState<Booking[]>([]);
@@ -71,26 +24,48 @@ export default function Home() {
     }
     fetchData();
   }, []);
-  return (
-    <div className="bg-slate-50 ">
-      <section>
-        <div className="container mx-auto py-10">
-          <h1>Bookings</h1>
-          <DataTable columns={bookingColumns} data={bookingData} />
+
+  if (!bookingData || !parcData || !userData) {
+    return <div>Loading...</div>;
+  } else
+    return (
+      <div className="bg-slate-50 ">
+        <div className="h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+          <section>
+            <div className="container mx-auto py-10">
+              <h1 className="mb-4">Bookings</h1>
+              <DataTable
+                columns={bookingColumns}
+                data={bookingData}
+                setData={setBookingData}
+                element={"Booking"}
+              />
+            </div>
+          </section>
+          <section>
+            <div className="container mx-auto py-10">
+              <h1 className="mb-4">Parcs</h1>
+              <DataTable
+                columns={parcColumns}
+                data={parcData}
+                setData={setParcData}
+                element={"Parc"}
+              />
+            </div>
+          </section>
+          <section>
+            <div className="container mx-auto py-10">
+              <h1 className="mb-4">Users</h1>
+              <DataTable
+                columns={userColumns}
+                data={userData}
+                setData={setUserData}
+                element={"User"}
+              />
+            </div>
+          </section>
         </div>
-      </section>
-      <section>
-        <div className="container mx-auto py-10">
-          <h1>Parcs</h1>
-          <DataTable columns={parcColumns} data={parcData} />
-        </div>
-      </section>
-      <section>
-        <div className="container mx-auto py-10">
-          <h1>Users</h1>
-          <DataTable columns={userColumns} data={userData} />
-        </div>
-      </section>
-    </div>
-  );
+        <ToastContainer />
+      </div>
+    );
 }
